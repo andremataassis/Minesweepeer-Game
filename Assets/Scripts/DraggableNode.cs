0d4e2e64db9c34321af5e3876707a69ebb36b5ff
AndreMAssis <andremataassis@gmail.com>
2026-08-10 16:56:35 -0400

    more code block progress

--- Assets/Scripts/DraggableNode.cs
+++ Assets/Scripts/DraggableNode.cs
@@ -5,6 +5,7 @@ public class DraggableNode : MonoBehaviour
     private Vector3 mOffset;
     private float mZCoord;
     private bool dragging = false;
+    public DropRoot current_root = null;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -40,12 +41,23 @@ public class DraggableNode : MonoBehaviour
 
     private void OnMouseUp()
     {
+        DeattachFromRoot();
         dragging = false;
     }
     public void AttachToRoot(DropRoot root)
     {
-        if (dragging == true) return;
+        if (dragging == true || current_root == root) return;
         transform.SetParent(root.transform);
         transform.position = root.GetComponent<Collider2D>().bounds.center;
+        current_root = root;
+        current_root.AttachNode(this);
+    }
+
+    public void DeattachFromRoot()
+    {
+        if (current_root == null) return;
+        transform.SetParent(null);
+        current_root.RemoveNode(this);
+        current_root = null;
     }
 }
--- Assets/Scripts/DropRoot.cs
+++ Assets/Scripts/DropRoot.cs
@@ -1,23 +1,57 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DropRoot : MonoBehaviour
 {
+    [SerializeField] public GameObject attach_zone_ref;
+    private BoxCollider2D collider;
+    public List<DraggableNode> nodes;
+
+    private void Awake()
+    {
+        collider = GetComponent<BoxCollider2D>();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        attach_zone_ref.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateColliderPosition();
+    }
+    
+    public void UpdateColliderPosition()
+    {
+        collider.offset = new Vector2(0, -1*nodes.Count - 1);
     }
     private void OnCollisionStay2D(Collision2D collision)
     {
         DraggableNode node = collision.gameObject.GetComponent<DraggableNode>();
         if (node == null) return;
 
+        attach_zone_ref.SetActive(true);
+        attach_zone_ref.transform.position = collider.bounds.center;
         node.AttachToRoot(this);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        attach_zone_ref.SetActive(false);
+    }
+
+    public void AttachNode(DraggableNode node)
+    {
+        if (node.current_root != this) return;
+        nodes.Add(node);
+    }
+
+    public void RemoveNode(DraggableNode node)
+    {
+        if (node.current_root != this) return;
+        nodes.Remove(node);
     }
 }
